fd9cceb4f234ec2863e12c8b50691c8623b63b55
MarcelNasr <136928799+MarcelNasr@users.noreply.github.com>
2023-12-27 19:10:01 +0200

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
-public class Hello {
-  public static void main (string[] args) {
+public class hello {
+  public static void main(String[] args) {
     // Prints "Hello, world" in the terminal window.
     System.out.println("Hello, World");
   }
